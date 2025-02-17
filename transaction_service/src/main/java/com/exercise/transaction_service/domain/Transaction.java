904360d960275b20a14747dba4feef82fa0f222f
Cristopher García <cristophergarcia@softwareevolutivo.com.ec>
2025-02-17 00:52:50 -0500

    Added lombok anotations

--- transaction_service/src/main/java/com/exercise/transaction_service/domain/Transaction.java
+++ transaction_service/src/main/java/com/exercise/transaction_service/domain/Transaction.java
@@ -2,16 +2,23 @@ package com.exercise.transaction_service.domain;
 
 import com.exercise.transaction_service.enums.TransactionType;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Entity
 @Table(name = "transaction")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     Long transactionId;
 
     @Column(nullable = false)
@@ -29,88 +36,4 @@ public class Transaction {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id", nullable = false)
     Account account;
-
-    public Transaction() {
-    }
-
-    public Transaction(Long transactionId, LocalDateTime transactionDate, TransactionType transactionType, Double amount, Double balance, Account account) {
-        this.transactionId = transactionId;
-        this.transactionDate = transactionDate;
-        this.transactionType = transactionType;
-        this.amount = amount;
-        this.balance = balance;
-        this.account = account;
-    }
-
-    public Long getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(Long transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public LocalDateTime getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setTransactionDate(LocalDateTime transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    public TransactionType getTransactionType() {
-        return transactionType;
-    }
-
-    public void setTransactionType(TransactionType transactionType) {
-        this.transactionType = transactionType;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public Double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(Double balance) {
-        this.balance = balance;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Transaction that = (Transaction) o;
-        return Objects.equals(transactionId, that.transactionId) && Objects.equals(transactionDate, that.transactionDate) && transactionType == that.transactionType && Objects.equals(amount, that.amount) && Objects.equals(balance, that.balance) && Objects.equals(account, that.account);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(transactionId, transactionDate, transactionType, amount, balance, account);
-    }
-
-    @Override
-    public String toString() {
-        return "Transaction{" +
-                "transactionId=" + transactionId +
-                ", transactionDate=" + transactionDate +
-                ", transactionType=" + transactionType +
-                ", amount=" + amount +
-                ", balance=" + balance +
-                ", account=" + account +
-                '}';
-    }
 }
